0b60ca9d11e011812d5180ea283cbafd418c175f
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2025-12-23 04:01:53 +0100

    1

---
+++ src/test/java/mate/academy/security/repository/CategoryRepositoryTest.java
@@ -0,0 +1,11 @@
+package mate.academy.security.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+}
