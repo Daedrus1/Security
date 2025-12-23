7f469fdd33a9c5a5a7faf32cfaf2f3961c5b5acb
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2025-12-23 21:01:14 +0100

    1

--- src/test/java/mate/academy/security/repository/BookRepositoryTest.java
+++ src/test/java/mate/academy/security/repository/BookRepositoryTest.java
@@ -1,4 +1,31 @@
 package mate.academy.security.repository;
 
+import java.math.BigDecimal;
+import mate.academy.security.model.Book;
+import mate.academy.security.model.Category;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void saveAndFindById_shouldWork(){
+        Category category = new Category();
+        category.setName("cat-for-book");
+        Category savedCategory = categoryRepository.save(category);
+
+        Book book = new Book();
+        book.setTitle("Book Title");
+        book.setAuthor("Author");
+        book.setPrice(BigDecimal.valueOf(100));
+
+    }
 }
